acf66caf63c39ee1ab09eea66aded101925bb950
yilz <90294263+yi1z@users.noreply.github.com>
2023-08-14 15:47:08 +0800

    Add files via upload
    P9

---
+++ IsPalindromeInteger.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x_str = str(x)
+
+        for i in range(len(x_str) // 2):
+            if x_str[i] != x_str[-i - 1]:
+                return False
+
+        return True
